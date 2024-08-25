d119939f1c0cc9b5245606a1317409e27f9b67e5
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-08-25 11:35:02 -1100

    first commit

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DisplaySection() {
+  return (
+    <div>
+      <h2 className="title">New</h2>
+      <p className="text">Brilliant</p>
+      <span className="description">
+        A display that's up to 2x brighter in the sun.
+      </span>
+    </div>
+  );
+}
+
+export default DisplaySection;
