e4efaefa0588bdc1f85c4fdc29cb5028578147e7
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-08-26 18:25:31 -1100

    new changes

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 function DisplaySection() {
   return (
-    <div>
+    <div className="display-section wrapper">
       <h2 className="title">New</h2>
-      <p className="text">Brilliant</p>
+      <p className="text">Brilliant.</p>
       <span className="description">
         A display that's up to 2x brighter in the sun.
       </span>
+      <button className="button">Try me</button>
+      <button className="back-button">TOP</button>
     </div>
   );
 }
---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,11 @@
+import React, {uswref} from "react";
+
+function WebgiViewer() {
+  return (
+    <div id="webgi-canvas-container">
+      <canvas id="webgi-canvas" />
+    </div>
+  );
+}
+
+export default WebgiViewer;
