be81e89cd44a017aa2862a167f13849dd29146ae
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-09-01 16:50:22 -1100

    Built 3D Model Working on Animation

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -20,19 +20,18 @@ import {
 } from "webgi";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { scrollAnimation } from "../lib/scroll-animaiton";
+import { scrollAnimation } from "../lib/scroll-animation";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function WebgiViewer() {
   const canvasRef = useRef(null);
 
-  const memorizedScrollAnimation = useCallback(position, target, onUpdate) => {
+  const memorizedScrollAnimation = useCallback((position, target, onUpdate) => {
     if (position && target && onUpdate) {
       scrollAnimation(position, target, onUpdate);
-
-    }, []
-  };
+    }
+  }, []);
 
   const setupViewer = useCallback(async () => {
     const viewer = new ViewerApp({
@@ -65,12 +64,19 @@ function WebgiViewer() {
 
     let needsUpdate = true;
 
+    const onUpdate = () => {
+      needsUpdate = true;
+      viewer.setDirty();
+    };
+
     viewer.addEventListener("preFrame", () => {
       if (needsUpdate) {
         camera.positionTargetUpdated(true);
         needsUpdate = false;
       }
     });
+
+    memorizedScrollAnimation(position, target, onUpdate);
   }, []);
 
   useEffect(() => {
